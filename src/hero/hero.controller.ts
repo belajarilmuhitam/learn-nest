29472df67868e0360dfd78308fa44053cc12ae8f
Chandra Halim <chan.halim98@gmail.com>
2024-03-28 11:04:09 -0700

    f

--- src/hero/hero.controller.ts
+++ src/hero/hero.controller.ts
@@ -1,15 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, Post, Req, Res } from '@nestjs/common';
 
 @Controller('hero')
 export class HeroController {
   @Get('index')
-  index() {
-    return 'hero index';
+  @HttpCode(200)
+  index(@Res() response): void {
+    const responseObject = {
+      title: 'hero index',
+      description: 'this is description',
+    };
+    return response.json(responseObject);
   }
 
   @Get('create')
-  create() {
+  create(@Res({ passthrough: true }) response): string {
+    response.cookie('name', 'baledemia');
     return 'create index';
+  }
+
+  @Post('store')
+  store(@Req() request) {
+    return {
+      data: request.body,
+    };
   }
 
   @Get('*')
